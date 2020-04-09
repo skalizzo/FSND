a0b210a33a5d3f0fa52debbd95adc00d233fbb6f
Christian Richter <christian00richter@gmail.com>
2020-04-09 13:32:19 +0200

    added check for permissions

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -104,21 +104,39 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+    
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+                        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+    return True
 
-    return wrapper
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            
+            check_permissions(permission, payload)
+            
+            return f(payload, *args, **kwargs)
+
+        return wrapper
 
 @app.route('/headers')
-@requires_auth
+@requires_auth('get:header')
 def headers(payload):
     print(payload)
     return 'Access Granted'
